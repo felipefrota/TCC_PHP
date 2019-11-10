e200018fd3ce69eac10285a5516f16cf3235b372
Felipe Frota <felipeoliveirafrota@gmail.com>
2019-11-10 14:58:35 -0300

    upando pagina instituicao.php atualizada

--- afterLogin/instituicao.php
+++ afterLogin/instituicao.php
@@ -26,16 +26,107 @@ if (!isset($_SESSION['usuario_instituicaoID']) OR ($_SESSION['tipo'] <$nivel_nec
 <html>
 <head>
  <title>Upload de imagens</title>
- <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=devide-width, initial-scale=1">
+    <link href="../Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../_CSS/styles.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 
 <body>
-<div class="container">
+
+<header>
+        <div id="">
+
+            <div class="Principal">
+                <!----------------------------------------------------------------------------------------->
+                <!--<object type="text/html" data="../nav-bar.html"></object>-->
+                <div id="Nav-Bar">
+
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <a href="../IndexProject/Index.html" class="navbar-brand">
+                                <!------------------------------------ Logo abaixo ----------------------------------------------------------->
+                                    <img src="../Images/logo.png">
+                                </a>
+                                <!------------------------------------ Fechando Logo ----------------------------------------------------------->
+
+
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuCelular" aria-controls="menu" aria-expanded="false" aria-label="Menu Colapso">
+                                    <span class="navbar-toggler-icon text-black"></span>
+                                </button>
+                            </div>
+
+                            <div id="menuCelular" class="collapse navbar-collapse">
+
+                                <ul class="navbar-nav ml-auto text-light nav-menu">
+                                    <li class="navbar-text"><a class="nav-link text-dark font-weight-bold" href="../Index/Index.php">Home</a></li>
+                                    <li class="navbar-text navHistorias"><a class="nav-link text-dark font-weight-bold" href="#historias">Historia</a></li>
+                                    <li class="navbar-text nav-instituicoes"><a class="nav-link text-dark font-weight-bold" href="../Institutions/Institutions.php">Instituições</a></li>
+                                    <li>
+                <!----------------------------------------------------------------------------------------->
+                <!---------------------------------Botao Saudação------------------------------------------>
+                                        <?php
+                                        if (isset($_SESSION["nomeUsuario_nomeFantasia"])) {
+                                            $user = $_SESSION["nomeUsuario_nomeFantasia"];
+
+                                            $saudacao = "SELECT nomeUsuario_nomeFantasia ";
+                                            $saudacao .= "FROM tb_usuario ";
+                                            $saudacao .= "WHERE usuario_instituicaoID = {$user} ";
+
+                                            $saudacao_login = mysqli_query($conecta, $saudacao);
+                                            if (!$saudacao_login) {
+                                                die("Falha no banco");
+                                            }
+
+                                            $saudacao_login = mysqli_fetch_assoc($saudacao_login);
+                                            $nome = $saudacao_login["nomeUsuario_nomeFantasia"];
+                                            ?>
+                                            <div class="dropdown nav-link">
+                                                <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <h8> Bem vindo, <?php echo $nome ?> </h8>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Action</a>
+                                                    <a class="dropdown-item" href="#">Another action</a>
+                                                    <a class="dropdown-item" href="../sair.php">Sair</a>
+                                                </div>
+                                            </div>
+                                        <?php
+                                        }
+                                        ?>
+
+                                    </li>
+            <!---------------------------------Fechando Saudação--------------------------------------->
+            <!----------------------------------------------------------------------------------------->
+                                    <!--Modal login ou senha invalido-->
+                                    <!-- <button type="button" class="btn btn-primary" data-toggle="modal"
+                                        data-target="#modalExemplo">
+                                        Abrir modal de demonstração
+                                    </button> -->
+
+                                    <!-- Modal -->
+
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+
+
+                </div>
+            </div>
+
+
+    </header>
+    <!--Fechando o Div:Nav-Bar-->
+
+
+    <!----------------------------------------------------------------------------------------->
+
+    
+<div class="container-fluid">
 <h2><strong>Perfil</strong></h2><hr>
 
 <form method="POST" enctype="multipart/form-data">
@@ -85,5 +176,15 @@ USuARIO
         }
         ?>
     </main>
-<body>
+
+    <!----------------------------------------------------------------------------------------->
+    <script src="https://unpkg.com/popper.js@1.15.0/dist/umd/popper.min.js"></script>
+    <script src="../Bootstrap/js/jquery-3.4.1.min.js "></script>
+    <script src="../Bootstrap/js/bootstrap.min.js"></script>
+    <script src="../Bootstrap/js/jquery-3.4.1.min.js"></script>
+    <script src="Index.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
 </html>
