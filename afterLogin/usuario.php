ce123bcf9dda27430032e6e7a211de24581f5080
Felipe Frota <felipeoliveirafrota@gmail.com>
2019-11-11 16:55:33 -0300

    adcionando menus nas telas de login usuario e login instituicao

--- afterLogin/usuario.php
+++ afterLogin/usuario.php
@@ -42,9 +42,9 @@ session_start();
 <body>
 
     <header>
-        <div id="">
+        <div id="Principal">
 
-            <div class="Principal">
+            <div class="background00">
                 <!----------------------------------------------------------------------------------------->
                 <!--<object type="text/html" data="../nav-bar.html"></object>-->
                 <div id="Nav-Bar">
@@ -67,9 +67,9 @@ session_start();
                             <div id="menuCelular" class="collapse navbar-collapse">
 
                                 <ul class="navbar-nav ml-auto text-light nav-menu">
-                                    <li class="navbar-text"><a class="nav-link text-dark font-weight-bold" href="../Index/Index.php">Home</a></li>
+                                    <li class="navbar-text"><a class="nav-link text-dark font-weight-bold" href="../afterLogin/usuario.php">Home</a></li>
                                     <li class="navbar-text navHistorias"><a class="nav-link text-dark font-weight-bold" href="#historias">Historia</a></li>
-                                    <li class="navbar-text nav-instituicoes"><a class="nav-link text-dark font-weight-bold" href="../Institutions/Institutions.php">Instituições</a></li>
+                                    <li class="navbar-text nav-instituicoes"><a class="nav-link text-dark font-weight-bold" href="../afterLogin/usuario_instituicao.php">Instituições</a></li>
                                     <li>
                 <!----------------------------------------------------------------------------------------->
                 <!---------------------------------Botao Saudação------------------------------------------>
@@ -130,6 +130,56 @@ session_start();
 
     <!----------------------------------------------------------------------------------------->
 
+        <!----------------------------------------------------------------------------------------->
+
+        <section class="background2">
+
+<div class="container-fluid">
+    <div id="historias">
+        <h1 class="text-center">Inicio da Historia</h1>
+        <p class="text-center">Proin quis sapien id mi fringilla pharetra. Nullam vitae sapien sit amet risus
+            elementum vehicula ac sed felis. Nullam molestie lobortis magna ac finibus. Aenean consequat
+            sagittis tempor. Morbi mollis nibh et turpis tincidunt, nec aliquam
+            sem tempor. Phasellus mattis ligula ac urna mollis, in porttitor justo faucibus. In efficitur vitae
+            metus tristique varius. Praesent id arcu auctor, ornare odio eget, aliquet urna. Nam a ornare dui.
+        </p>
+
+        <p class="text-center"> Pellentesque rutrum vulputate risus, in placerat ex facilisis vitae. Quisque
+            hendrerit rutrum lorem, vitae venenatis velit porta non. Suspendisse at urna dapibus, volutpat risus
+            at, lobortis ex. Morbi et nisl eget arcu ultrices rutrum.
+            Praesent et enim et orci euismod mattis. Mauris rhoncus, sem sit amet porttitor condimentum, metus
+            ex laoreet lorem, ac blandit nisi libero a diam. Aliquam erat volutpat. Mauris faucibus lorem eu
+            imperdiet porta. Etiam vitae egestas
+            nisl. Curabitur ac erat nunc. Morbi dictum blandit felis, a feugiat ligula luctus eget. Sed pharetra
+            fringilla placerat. In facilisis auctor risus. Nulla vel lectus sapien. Pellentesque vel mauris
+            libero. Mauris placerat dictum diam
+            sed tincidunt.</p>
+
+        <p class="text-center"> Fusce ac libero non justo volutpat venenatis in non elit. Cras fermentum
+            ultricies rhoncus. Aliquam nec nisl ut erat congue faucibus et at ex. Nunc rhoncus nisi leo, in
+            sagittis tellus ullamcorper id. Sed sit amet massa lacinia, euismod
+            nulla in, gravida leo. Etiam cursus porta felis ut tempus. Phasellus tincidunt finibus nulla et
+            laoreet. Nunc est ex, pretium at odio sit amet, euismod volutpat ex. Vestibulum et justo at libero
+            imperdiet gravida. Fusce sed egestas
+            enim. Pellentesque euismod, sapien eget pellentesque ornare, sem sem blandit neque, vel consequat
+            dolor mauris in diam. Etiam accumsan id nisl ut dignissim. Nullam ac vulputate sem. Etiam et nunc
+            pharetra dui viverra venenatis in ac
+            dolor. Sed maximus urna a volutpat egestas. Vivamus in interdum nisi.</p>
+
+        <p class="text-center"> Etiam non scelerisque neque. Donec tincidunt sollicitudin neque, et fermentum
+            orci imperdiet a. Praesent sagittis ultricies elit eu consectetur. Nam non molestie ligula. Aenean
+            ornare feugiat leo, ut porta ante volutpat sed. Fusce porttitor
+            bibendum justo nec suscipit. Donec mattis tristique nulla sed viverra. Suspendisse id turpis a nunc
+            sodales faucibus. Proin eu ligula feugiat, malesuada velit vitae, tempus lacus. Nulla finibus
+            volutpat urna, et egestas mi egestas
+            eget. Phasellus urna ligula, finibus vitae lacinia quis, cursus ac nunc.</p>
+
+        <p class="text-center"> Cras a fermentum massa. Pellentesque suscipit ut mauris at ultrices. Nulla
+            malesuada eget leo ac condimentum. Maecenas ultrices neque nibh, at dictum libero pulvinar at.
+            Praesent euismod sagittis neque, eget eleifend nisl accumsan a. Suspendisse
+            euismod tristique lorem, ac dictum urna mattis id. Sed nec lectus dui.</p>
+    </div>
+</div>
 
 
     <main>
