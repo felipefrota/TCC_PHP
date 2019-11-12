0935313b07e1598c4cc610a6cff897f12d55bf0d
Felipe <felipeoliveirafrota@gmail.com>
2019-11-12 10:30:06 -0300

    arruando os codigos da pagina do vinicius

--- afterLogin/instituicao.php
+++ afterLogin/instituicao.php
@@ -26,6 +26,11 @@ if (!isset($_SESSION['usuario_instituicaoID']) or ($_SESSION['tipo'] < $nivel_ne
 <head>
     <meta charset="utf-8">
     <title>Alguma Coisa Lfie</title>
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=devide-width, initial-scale=1">
+
+    <link href="../Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../_CSS/styles.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -128,6 +133,14 @@ if (!isset($_SESSION['usuario_instituicaoID']) or ($_SESSION['tipo'] < $nivel_ne
             });
         });
     </script>
+
+
+      <!----------------------------------------------------------------------------------------->
+      <script src="https://unpkg.com/popper.js@1.15.0/dist/umd/popper.min.js"></script>
+    <script src="../Bootstrap/js/jquery-3.4.1.min.js "></script>
+    <!-- <script src="../Bootstrap/js/bootstrap.min.js"></script> -->
+    <script src="../Bootstrap/js/jquery-3.4.1.min.js"></script>
+    <script src="Index.js" type="text/javascript"></script>
 </body>
 
 </html>
